e803949c63b4f855cc5253a4bec1a3d1bff47b6b
alelevente <ale.levente@gmail.com>
2018-12-01 22:36:57 +0100

    Szakdolgozat prestate. MessengerSystem commented, refactored

--- src/microsim/devices/MessagingSystem/MessengerSystem.h
+++ src/microsim/devices/MessagingSystem/MessengerSystem.h
@@ -9,18 +9,31 @@
 #include <map>
 #include "Messenger.h"
 
+
+/// @brief Singleton class responsible for managing the messenger system.
 class MessengerSystem {
     friend class MessagingProxy;
     friend class Messenger;
+    //to be singleton, to following are private:
     MessengerSystem();
     ~MessengerSystem();
     MessengerSystem(MessengerSystem const&);
     void operator=(const MessengerSystem&);
+
+    /// @brief list of messenger entites
     std::map<std::string, Messenger*> messengerMap;
 
 public:
+    /// @brief for accessing this singleton class
     static MessengerSystem& getInstance();
+    /**
+     * @brief This method is called when a new intelligent agent was created (eg. a smart car)
+     * @param id ID of the new agent
+     * @param myVeh Vehicle of the new agent
+     * @param mySAL Simulation Abstraction Device of the new agent
+     */
     void addNewMessengerAgent(const std::string &id, SUMOVehicle *myVeh, MSDevice_SAL *mySAL);
+    /// @brief removes an intelligent agent's messenger interpretation which is given by its ID
     void removeMessengerAgent(const std::string &id);
 };
 
