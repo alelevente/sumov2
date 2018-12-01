23ebefef91b7afaf1b27a9638b3fd18d33863ad7
alelevente <ale.levente@gmail.com>
2018-12-01 23:09:29 +0100

    Szakdolgozat prestate. LCManager commented, refactored

--- src/microsim/devices/Others/LCManager.cpp
+++ src/microsim/devices/Others/LCManager.cpp
@@ -17,7 +17,6 @@ void LCManager::setIsMember(Group* group) {
     SUMOVehicle* holder = myLC->getMyVehicle();
     MessagingProxy::getInstance().getGroupOf(holder->getID());
     libsumo::Vehicle::setLaneChangeMode(holder->getID(), 512);
-    //libsumo::Vehicle::setLaneChangeMode(holder->getID(), 0);
     auto carId = group->carIDs.begin();
 
     for (auto i = group->LCFifo.begin(); i != group->LCFifo.end(); ++i) {
@@ -28,7 +27,6 @@ void LCManager::setIsMember(Group* group) {
 
 void LCManager::setIsLeader() {
     myLC->setMyGroupState(LEADER);
-    //libsumo::Vehicle::changeLaneRelative(myLC->getMyVehicle()->getID(), 0, 0);
     libsumo::Vehicle::setLaneChangeMode(myLC->getMyVehicle()->getID(), 1621);
     libsumo::Vehicle::setSpeed(myLC->getMyVehicle()->getID(), -1);
     requestedChanges = 0;
@@ -45,7 +43,6 @@ void LCManager::leaveGroup() {
     libsumo::Vehicle::setSpeed(ID, -1);
     requestedChanges = 0;
     hasChanged = 0;
-    //std::cout << ID << ": will continue" << std::endl;
 }
 
 void LCManager::groupChanging(MSLCM_SmartSL2015 *follower) {
@@ -58,9 +55,7 @@ void LCManager::groupChanging(MSLCM_SmartSL2015 *follower) {
 }
 
 void LCManager::hasToChange(int direction) {
-    //std::cout << myLC->getMyVehicle()->getID() << " has to change: " << direction << std::endl;
     libsumo::Vehicle::changeLaneRelative(myLC->getMyVehicle()->getID(), direction, 1000);
-    //libsumo::Vehicle::changeSublane(myLC->getMyVehicle()->getID(), direction*2.75);
     myLC->setOffset(direction);
     lastDir = direction;
     ++requestedChanges;
@@ -69,57 +64,10 @@ void LCManager::hasToChange(int direction) {
 
 void LCManager::changed() {
     if (requestedChanges>hasChanged) ++hasChanged;
-    //myLC->lineUpForCenterOfLane();
-}
-
-void LCManager::synch() {
-    //std::cout << libsumo::Simulation::getCurrentTime()-lastChange << std::endl;
-    if (libsumo::Simulation::getCurrentTime()-lastChange > 3500) {
-        //std::cout << myLC->getMyVehicle()->getID() << ": has resent the request of " << lastDir << std::endl;
-       /* if (myLC->getMyVehicle()->isSelected()) std::cout <<
-                                                          myLC->getMyVehicle()->getID() <<": " << hasChanged << "/" << requestedChanges << std::endl;*/
-        if ((hasChanged != requestedChanges) && (lastDir!=0)) {
-        // ->    libsumo::Vehicle::changeLaneRelative(myLC->getMyVehicle()->getID(), lastDir, 0);
-            //libsumo::Vehicle::changeSublane(myLC->getMyVehicle()->getID(), lastDir*2.75);
-          //  std::cout << myLC->getMyVehicle()->getID() << "is reset" << std::endl;
-        }
-        lastChange = libsumo::Simulation::getCurrentTime();
-    }
 }
 
 
 void LCManager::groupChanged() {
-    /*if (LCFifo.size() != 0 && carIDs.size() != 0) {
-        //MSVehicleControl::loadedVehBegin()
-        //std::vector<std::string> ids = libsumo::Vehicle::getIDList();
-
-        MSVehicleControl& vc = MSNet::getInstance()->getVehicleControl();
-        bool benne = vc.getVehicle(*carIDs[0]) != 0;
-       /* for (auto x= vc.loadedVehBegin(); x!=vc.loadedVehEnd() && !benne; ++x) {
-            if ((*x).first == *carIDs[0]) benne = true;
-        }*/
-       /* if (!benne) {
-
-            delete *carIDs.begin();LCFifo.erase(LCFifo.begin());
-            carIDs.erase(carIDs.begin());
-            return;
-        }*/
-        //std::cout << myLC->getMyVehicle()->getID() << " has FIFO of: ";
-        // for (auto i = LCFifo.begin(); i != LCFifo.end(); ++i)
-         //   std::cout << (*i)->getMyVehicle()->getID();
-        //std::cout << std::endl;
-      /*  std::string ID = LCFifo[0]->getMyVehicle()->getID();
-        libsumo::Vehicle::setLaneChangeMode(ID, 1621);
-        libsumo::Vehicle::setSpeed(ID, -1);*/
-        //libsumo::Vehicle::changeLaneRelative(ID, 0, 1);
-        //std::cout << ID << ": may continue" << std::endl;
-
-     /*   delete *carIDs.begin();LCFifo.erase(LCFifo.begin());
-        carIDs.erase(carIDs.begin());
-    }*/
-
-   // std::cout << MSNet::getInstance()->getVehicleControl().getRunningVehicleNo();
-
     if (myLC->myFollower != nullptr) {
         std::string ID = myLC->myFollower->getID();
         libsumo::Vehicle::setLaneChangeMode(ID, 1621);
@@ -131,6 +79,4 @@ void LCManager::blocker(MSLCM_SmartSL2015 *who) {
     std::string ID = who->getMyVehicle()->getID();
     libsumo::Vehicle::setSpeed(ID, 0);
     libsumo::Vehicle::setLaneChangeMode(ID, 512);
-    //libsumo::Vehicle::changeLaneRelative(ID, 0, 60000);
-    //std::cout << who->getMyVehicle()->getID() << ": is blocker" << std::endl;
 }
--- src/microsim/devices/Others/LCManager.h
+++ src/microsim/devices/Others/LCManager.h
@@ -13,23 +13,42 @@
 class MSLCM_SmartSL2015;
 class Group;
 
+/// @brief This class is responsible for managing lane change manouvers in the simulation abstraction layer
 class LCManager {
-    MSLCM_SmartSL2015* letUsIn = nullptr, *myLC;
+    /// @brief the smart car's lane change model
+    MSLCM_SmartSL2015 *myLC;
+    /// @brief First-in-First-out list of the group leaders who made possible our lane changes (pointer representation)
     std::vector<MSLCM_SmartSL2015*> LCFifo;
+    /// @brief First-in-First-out list of the group leaders who made possible our lane changes (string, ID representation)
     std::vector<std::string*> carIDs;
-    int lastDir = 0, hasChanged = 0, requestedChanges = 0;
-    SUMOTime  lastChange = 0;
+    /// @brief number of absolved lane changes
+    int hasChanged = 0;
+    /// @brief number of requested lane changes
+    int requestedChanges = 0;
+    /// @brief direction of last requested lane change
+    int lastDir = 0;
+    /// @brief timestamp of last requested lane change
+    SUMOTime lastChange = 0;
+
 public:
+    /// @brief constructor
     LCManager(MSLCM_SmartSL2015* myLC);
+    /// @brief inform LCManager about the changes in group membership
     void setIsMember(Group* group);
+    /// @brief inform LCManager if the smart car left the group
     void leaveGroup();
+    /// @brief inform LCManager that the car it controls has become a group leader
     void setIsLeader();
+    /// @brief infrom LCManager that a lane change is needed in the given direction
     void hasToChange(int direction);
+    /// @brief updates the Fifos
     void groupChanging(MSLCM_SmartSL2015* follower);
+    /// @brief called when the last car of a group has finished its lane change
     void groupChanged();
+    /// @brief used to inform a group leader that it is now a blocker, so it has to let in another group
     static void blocker(MSLCM_SmartSL2015* who);
+    /// @brief called, when the controlled car changed its change of lane
     void changed();
-    void synch();
 };
 
 
